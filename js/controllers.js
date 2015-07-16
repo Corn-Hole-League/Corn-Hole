75cc992a8b380d00eae06d2050d2b5a53709dc76
Trevor Glass <glasser1988@gmail.com>
2015-07-16 13:38:26 -0400

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+  angular
+    .module('cornHole')
+    .controller('MainController', function ($scope, $location) {
+      $scope.hello = "hello world";
+    });
+
+
+})();
